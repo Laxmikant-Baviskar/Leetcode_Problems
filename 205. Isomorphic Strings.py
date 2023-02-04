09921a357b8db930eb92ac3df2c81d89f0ac9d8d
Laxmikant-Baviskar <99134289+Laxmikant-Baviskar@users.noreply.github.com>
2023-02-04 23:47:52 +0530

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -6,7 +6,7 @@
 
 # All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
-# ==========================================
+# ===========================================
 # s = "party"
 # t = "theer"
 # s = "egg"
